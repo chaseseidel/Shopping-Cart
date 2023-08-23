5feedc414326793aaf6e1f40a3069dd5ed3f31e5
chaseseidel <chaseseidel16@gmail.com>
2023-08-22 22:12:13 -0700

    Create icon component

---
+++ src/components/Icon.jsx
@@ -0,0 +1,9 @@
+function Icon({ onClick = "", src, alt }) {
+  return (
+    <button onClick={onClick} className="header-btn">
+      <img src={src} alt={alt} className="icon" />
+    </button>
+  );
+}
+
+export default Icon;
