40fa458c08e670480ead111596910727836cd4cf
chaseseidel <chaseseidel16@gmail.com>
2023-08-24 16:15:24 -0700

    Add category buttons

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -5,11 +5,18 @@ import Account from "../assets/images/account.png";
 import Favorite from "../assets/images/favorite.png";
 import Cart from "../assets/images/cart.png";
 import Searchbar from "../components/SearchBar";
+import Button from "../components/Button";
 
 function Header() {
   return (
     <header>
       <Logo />
+      <ul className="categories">
+        <Button text="Men" style="category" />
+        <Button text="Women" style="category" />
+        <Button text="Jewelry" style="category" />
+        <Button text="Electronics" style="category" />
+      </ul>
       <ul className="header-right">
         <Searchbar src={Search} alt="Search" />
         <Icon src={Account} alt="Profile" />
