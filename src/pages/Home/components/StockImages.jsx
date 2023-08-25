5917d62eff00d1251a85057bd25aa28d7e442f2f
chaseseidel <chaseseidel16@gmail.com>
2023-08-25 16:41:11 -0700

    Integrate stockImage component

--- src/pages/Home/components/StockImages.jsx
+++ src/pages/Home/components/StockImages.jsx
@@ -1,19 +1,17 @@
 import stock1 from "../../../assets/images/stock-1.jpg";
 import stock2 from "../../../assets/images/stock-2.jpg";
 import stock3 from "../../../assets/images/stock-3.jpg";
+import StockImage from "./StockImage";
+import { useNavigate } from "react-router-dom";
 
 function StockImages() {
+  const navigate = useNavigate();
+
   return (
     <ul className="stock-images">
-      <li>
-        <img src={stock1} alt="" className="stock-img" />
-      </li>
-      <li>
-        <img src={stock2} alt="" className="stock-img" />
-      </li>
-      <li>
-        <img src={stock3} alt="" className="stock-img" />
-      </li>
+      <StockImage image={stock1} onClick={() => navigate("/jewelry")} />
+      <StockImage image={stock2} onClick={() => navigate("/men")} />
+      <StockImage image={stock3} onClick={() => navigate("/women")} />
     </ul>
   );
 }
