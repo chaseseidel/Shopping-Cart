54e442f63b0b3c132d72c0b8bf46267885327c76
chaseseidel <chaseseidel16@gmail.com>
2023-08-25 16:43:13 -0700

    Create cart state and functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
+import Header from "./pages/Header/Header";
+import Cart from "./pages/Cart/Cart";
+import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+
 function App() {
+  const [cartActive, setCartActive] = useState(false);
+  const [cart, setCart] = useState([]);
+
+  const toggleCart = () => {
+    setCartActive(!cartActive);
+  };
+
+  const addItemToCart = (product) => {
+    const newCart = [...cart];
+    const foundIndex = newCart.findIndex((item) => item.id === product.id);
+    foundIndex > -1
+      ? newCart[foundIndex].quantity++
+      : newCart.push({ product, quantity: 1, id: product.id });
+    setCart(newCart);
+    toggleCart();
+  };
+
+  const removeItemFromCart = (product) => {
+    const newCart = [...cart];
+    const productIndex = newCart.findIndex((item) => item.id === product.id);
+    newCart.splice(productIndex, 1);
+    setCart(newCart);
+  };
+
+  const editQuantity = (cartItem, value) => {
+    const newCart = [...cart];
+    const productIndex = newCart.findIndex((item) => item.id === cartItem.id);
+    value
+      ? newCart[productIndex].quantity++
+      : cartItem.quantity > 1 && newCart[productIndex].quantity--;
+    setCart(newCart);
+  };
+
   return (
     <>
-      <div>Hello World</div>
+      <Header cartClick={toggleCart} />
+      <Outlet context={[addItemToCart]} />
+      {cartActive && (
+        <Cart
+          isActive={cartActive}
+          cart={cart}
+          exitCart={toggleCart}
+          removeItem={removeItemFromCart}
+          editQuantity={editQuantity}
+        />
+      )}
     </>
   );
 }
