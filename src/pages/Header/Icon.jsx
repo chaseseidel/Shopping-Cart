2ea2eeacd7e6b18cb0e5d13cc911bbd8d212b9b0
chaseseidel <chaseseidel16@gmail.com>
2023-08-25 16:42:05 -0700

    Add children prop

--- src/pages/Header/Icon.jsx
+++ src/pages/Header/Icon.jsx
@@ -1,7 +1,8 @@
-function Icon({ onClick = () => {}, src, alt }) {
+function Icon({ children, onClick = () => {}, src, alt }) {
   return (
     <button onClick={onClick} className="header-btn">
       <img src={src} alt={alt} className="icon" />
+      {children}
     </button>
   );
 }
