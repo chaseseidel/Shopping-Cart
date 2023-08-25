6cfc3821b18567d4918c58820260e4c29d043c81
chaseseidel <chaseseidel16@gmail.com>
2023-08-24 19:44:13 -0700

    Create home page component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import StockImages from "./components/StockImages";
+import Advertisement from "./components/Advertisement";
+import "./home.css";
+
+function Home() {
+  return (
+    <main>
+      <Advertisement />
+      <StockImages />
+    </main>
+  );
+}
+
+export default Home;
