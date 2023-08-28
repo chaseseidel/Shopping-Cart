68408670657572ab405faa415fc72f5a63c91f80
chaseseidel <chaseseidel16@gmail.com>
2023-08-28 15:34:28 -0700

    Add product, networkError, and addItem components

--- src/pages/Pages/WomensPage.jsx
+++ src/pages/Pages/WomensPage.jsx
@@ -1,24 +1,25 @@
 import Product from "./Product";
-import useWomensProducts from "../../hooks/useWomensProducts";
+import useWomensProducts from "../../hooks/useWomensProducts.js";
 import LoadingPage from "../LoadingPage";
+import { useOutletContext } from "react-router-dom";
+import NetworkError from "./NetworkError";
 
 function WomensPage() {
   const { products, error, loading } = useWomensProducts();
-
-  console.log(products);
+  const [addItem] = useOutletContext();
 
   if (loading) {
     return <LoadingPage />;
   }
 
   if (error) {
-    return <div className="loading">A network error was encountered.</div>;
+    return <NetworkError />;
   }
 
   return (
     <main className="products">
       {products.map((product) => (
-        <Product product={product} key={product.id} />
+        <Product product={product} addItem={addItem} key={product.id} />
       ))}
     </main>
   );
