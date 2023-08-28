b2da346b6a2865c9d6a1f32251aaa9011acc6e28
chaseseidel <chaseseidel16@gmail.com>
2023-08-28 15:07:55 -0700

    Add animation with framer motion

--- src/pages/Home/components/Advertisement.jsx
+++ src/pages/Home/components/Advertisement.jsx
@@ -1,10 +1,16 @@
 import Electronics from "../../../assets/images/electronics.png";
 import Button from "../../../components/Button";
+import { motion } from "framer-motion";
 
 function Advertisement() {
   return (
     <div className="ad">
-      <div className="ad-left-container">
+      <motion.div
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ bounce: 0 }}
+        className="ad-left-container"
+      >
         <h2>
           EXTRA 20% OFF
           <br />
@@ -16,10 +22,15 @@ function Advertisement() {
           Exclusions Apply.
         </p>
         <Button text="Shop" style="shop" />
-      </div>
-      <div className="ad-right-container">
+      </motion.div>
+      <motion.div
+        initial={{ x: 100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ bounce: 0 }}
+        className="ad-right-container"
+      >
         <img src={Electronics} alt="" className="stock-img" />
-      </div>
+      </motion.div>
     </div>
   );
 }
