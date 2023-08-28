6df4678846e00e8ab0c0dadef833004e85e2fa70
chaseseidel <chaseseidel16@gmail.com>
2023-08-28 15:10:12 -0700

    Add path to men's page

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./assets/reset.css";
+import "./assets/styles.css";
 import App from "./App";
+import Home from "./pages/Home/Home";
+import ErrorPage from "./pages/ErrorPage";
+import MensPage from "./pages/Pages/MensPage";
+import WomensPage from "./pages/Pages/WomensPage";
 
 function Router() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "/men", element: <MensPage /> },
+      ],
+      errorElement: <ErrorPage />,
     },
   ]);
 
